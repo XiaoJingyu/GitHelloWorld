a68cb5215cf9c98fa4248b47c05f7f5c6a7332eb
Xiao Jinyu <wurong1105@126.com>
2015-04-06 14:45:28 +0800

    首次提交到本地仓库

---
+++ src/GitMain.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C), 2002-2015, 苏宁易购电子商务有限公司
+ * FileName: GitMain.java
+ * Author:   14041614
+ * Date:     2015-4-6 下午2:41:59
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+
+public class GitMain {
+    public static void main(String[] args) {
+        System.out.println("First git project.");
+    }
+}
